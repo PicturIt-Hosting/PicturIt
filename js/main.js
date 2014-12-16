aca9a9cb8bac4e9dbd09a429954ea94f6db0f376
MegaApuTurkUltra <apu@aputurk.tk>
2014-12-16 18:44:37 -0500

    Swaggified background

--- js/main.js
+++ js/main.js
@@ -1,37 +1,139 @@
-$("form#image_upload").submit(function(e){
+/**
+ * Canvas background
+ */
+///// http://thecodeplayer.com/walkthrough/html5-canvas-snow-effect
+var canvas = $("canvas#background").get(0);
+var ctx = canvas.getContext("2d");
+
+//canvas dimensions
+var W = $(window).width();
+var H = $(window).height();
+canvas.width = W;
+canvas.height = H;
+
+//snowflake particles
+var mp = 50; //max particles
+var particles = [];
+for (var i = 0; i < mp; i++) {
+	particles.push({
+		x : Math.random() * W, //x-coordinate
+		y : Math.random() * H, //y-coordinate
+		r : Math.random() * 4 + 1, //radius
+		d : Math.random() * mp //density
+	})
+}
+
+//Lets draw the flakes
+function draw() {
+	ctx.clearRect(0, 0, W, H);
+
+	ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+	ctx.beginPath();
+	for (var i = 0; i < mp; i++) {
+		var p = particles[i];
+		ctx.moveTo(p.x, p.y);
+		ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2, true);
+	}
+	ctx.fill();
+	update();
+}
+
+//Function to move the snowflakes
+//angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
+var angle = 0;
+function update() {
+	angle += 0.01;
+	for (var i = 0; i < mp; i++) {
+		var p = particles[i];
+		//Updating X and Y coordinates
+		//We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
+		//Every particle has its own density which can be used to make the downward movement different for each flake
+		//Lets make it more random by adding in the radius
+		p.y += Math.cos(angle + p.d) + 1 + p.r / 2;
+		p.x += Math.sin(angle) * 2;
+
+		//Sending flakes back from the top when it exits
+		//Lets make it a bit more organic and let flakes enter from the left and right also.
+		if (p.x > W + 5 || p.x < -5 || p.y > H) {
+			if (i % 3 > 0) //66.67% of the flakes
+			{
+				particles[i] = {
+					x : Math.random() * W,
+					y : -10,
+					r : p.r,
+					d : p.d
+				};
+			} else {
+				//If the flake is exitting from the right
+				if (Math.sin(angle) > 0) {
+					//Enter from the left
+					particles[i] = {
+						x : -5,
+						y : Math.random() * H,
+						r : p.r,
+						d : p.d
+					};
+				} else {
+					//Enter from the right
+					particles[i] = {
+						x : W + 5,
+						y : Math.random() * H,
+						r : p.r,
+						d : p.d
+					};
+				}
+			}
+		}
+	}
+}
+
+//animation loop
+setInterval(draw, 33);
+
+$(window).resize(function(){
+	W = $(window).width();
+	H = $(window).height();
+	canvas.width = W;
+	canvas.height = H;
+});
+
+/**
+ * Image uploading via ajax
+ */
+$("form#image_upload").submit(function (e) {
 	e.preventDefault();
-    var formData = new FormData($(this)[0]);
+	var formData = new FormData($(this)[0]);
 	$("#upload_progress").show();
-    $.ajax({
-        url: "upload.php",
-        type: 'POST',
-        data: formData,
-        async: true,
-		xhr: function(){
+	$.ajax({
+		url : "upload.php",
+		type : 'POST',
+		data : formData,
+		async : true,
+		xhr : function () {
 			var xhr = jQuery.ajaxSettings.xhr();
-            if(xhr instanceof window.XMLHttpRequest) {
-                xhr.upload.addEventListener("progress", function(evt){
-					if (evt.lengthComputable) {  
+			if (xhr instanceof window.XMLHttpRequest) {
+				xhr.upload.addEventListener("progress", function (evt) {
+					if (evt.lengthComputable) {
 						var percentComplete = evt.loaded * 100 / evt.total;
 						console.log("Progress: ", percentComplete);
-						$("#upload_progress").attr("max","100").attr("value",percentComplete);
+						$("#upload_progress").attr("max", "100").attr("value", percentComplete);
 					}
-				}, false); 
-            }
-            return xhr;
+				}, false);
+			}
+			return xhr;
 		},
-        success: function (data) {
-            $("#upload_progress").hide();
+		success : function (data) {
+			$("#upload_progress").hide();
 			$("#json_response").text(JSON.stringify(data));
 			$("[name=userfile]").val("");
-        },
-		error: function(){
+		},
+		error : function () {
 			$("#upload_progress").hide();
 			$("#json_response").text("Sorry! Something bad happened and your image wasn't uploaded. Check your internet connection and try again.");
 		},
-        cache: false,
-        contentType: false,
-        processData: false
-    });
-    return false;
+		cache : false,
+		contentType : false,
+		processData : false
+	});
+	return false;
 });
