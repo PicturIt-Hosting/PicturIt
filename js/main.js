cda63e878192b5e05af27640ecc7d46e285f1e0b
MegaApuTurkUltra <apu@aputurk.tk>
2014-12-15 22:24:53 -0500

    Updated index page to use h5bp

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-
+$("form#image_upload").submit(function(e){
+	e.preventDefault();
+    var formData = new FormData($(this)[0]);
+	$("#upload_progress").show();
+    $.ajax({
+        url: "upload.php",
+        type: 'POST',
+        data: formData,
+        async: true,
+		xhr: function(){
+			var xhr = jQuery.ajaxSettings.xhr();
+            if(xhr instanceof window.XMLHttpRequest) {
+                xhr.upload.addEventListener("progress", function(evt){
+					if (evt.lengthComputable) {  
+						var percentComplete = evt.loaded * 100 / evt.total;
+						console.log("Progress: ", percentComplete);
+						$("#upload_progress").attr("max","100").attr("value",percentComplete);
+				}
+				}, false); 
+            }
+            return xhr;
+		},
+        success: function (data) {
+            $("#upload_progress").hide();
+			$("#json_response").text(JSON.stringify(data));
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+    return false;
+});
